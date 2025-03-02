f804b8bf7ad323d76657373b448dd2c0fb1e6048
Trevin Chow <trevin@trevinchow.com>
2025-03-02 14:49:15 -0800

    feat: homepage slide show feature

--- source/javascripts/home-carousel.js
+++ source/javascripts/home-carousel.js
@@ -1,10 +1,15 @@
-const homeSlideshowContainer = document.querySelector('.home-carousel');
+const homeSlideshowContainer = document.querySelector('.home-slideshow');
 if (homeSlideshowContainer) {
   document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.home-carousel', {
+    var splide = new Splide( '.home-slideshow', {
       arrows: false,
       type: 'slide',
+      autoplay: themeOptions.homepageSlideshowAutoplay,
+      interval: themeOptions.homepageSlideshowSpeed,
+      speed: 1500,
+      rewind: true,
       keyboard: true,
+      pagination: false
     } );
     splide.mount();
   });
