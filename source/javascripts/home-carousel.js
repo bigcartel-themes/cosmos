87954d6d9ddf1ad99025db9c9f9a4f770f0b319d
Trevin Chow <trevin@bigcartel.com>
2025-08-10 13:11:45 -0700

    fix: slideshow proper size when 1 image

--- source/javascripts/home-carousel.js
+++ source/javascripts/home-carousel.js
@@ -1,6 +1,15 @@
 const homeSlideshowContainer = document.querySelector('.splide.home-slideshow');
 if (homeSlideshowContainer) {
   function initSplide() {
+    const slideCount = homeSlideshowContainer.querySelectorAll('.splide__slide').length;
+    
+    // Add class to differentiate single vs multiple slides
+    if (slideCount === 1) {
+      homeSlideshowContainer.classList.add('home-slideshow--single');
+    } else {
+      homeSlideshowContainer.classList.add('home-slideshow--multiple');
+    }
+    
     var splide = new Splide( '.splide.home-slideshow', {
       arrows: true,
       pagination: true,
